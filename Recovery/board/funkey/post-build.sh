542e735465726fd0d30b974ae4358ab8523ae9e2
Spaceghost <Spaceghost@users.noreply.github.com>
2026-08-27 06:25:47 -0700

    build: make post-build scripts safe and repeatable
    (cherry picked from commit 64cfdea5a8031759c3ce7c4efde17d423a18e6d2)

--- Recovery/board/funkey/post-build.sh
+++ Recovery/board/funkey/post-build.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
+set -e
+
 # Add local path to init scripts
-sed -i '3iexport PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin' ${TARGET_DIR}/etc/init.d/rcK
-sed -i '3iexport PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin' ${TARGET_DIR}/etc/init.d/rcS
+path_line='export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin'
+for script in "${TARGET_DIR}/etc/init.d/rcK" "${TARGET_DIR}/etc/init.d/rcS"; do
+	if ! grep -qxF "$path_line" "$script"; then
+		sed -i "3i$path_line" "$script"
+	fi
+done
 
 # Remove log daemon init scripts since they are loaded from inittab
 rm -f ${TARGET_DIR}/etc/init.d/S01syslogd ${TARGET_DIR}/etc/init.d/S02klogd
 
 # Remove dhcp lib dir and link to /tmp
-rm -rf ${TARGET_DIR}/var/lib/dhcp/
-ln -s /tmp ${TARGET_DIR}/var/lib/dhcp
+rm -rf "${TARGET_DIR}/var/lib/dhcp"
+ln -s /tmp "${TARGET_DIR}/var/lib/dhcp"
 
 # Remove dhcpcd dir and link to /tmp
-rm -rf ${TARGET_DIR}/var/db/dhcpcd/
-ln -s /tmp ${TARGET_DIR}/var/db/dhcpcd
+mkdir -p "${TARGET_DIR}/var/db"
+rm -rf "${TARGET_DIR}/var/db/dhcpcd"
+ln -s /tmp "${TARGET_DIR}/var/db/dhcpcd"
 
 # Change dropbear init sequence
-mv ${TARGET_DIR}/etc/init.d/S50dropbear ${TARGET_DIR}/etc/init.d/S42dropbear
+if [ -e "${TARGET_DIR}/etc/init.d/S50dropbear" ]; then
+	mv "${TARGET_DIR}/etc/init.d/S50dropbear" "${TARGET_DIR}/etc/init.d/S42dropbear"
+fi
 
 # Redirect drobear keys to /tmp
-rm -rf ${TARGET_DIR}/etc/dropbear
-ln -s /tmp ${TARGET_DIR}/etc/dropbear
+rm -rf "${TARGET_DIR}/etc/dropbear"
+ln -s /tmp "${TARGET_DIR}/etc/dropbear"
 
 # Generate U-Boot environment for app
 ${HOST_DIR}/bin/mkenvimage -p 0x0 -s 0x20000 -o ${BINARIES_DIR}/u-boot-env.img ${TARGET_DIR}/etc/u-boot.env
