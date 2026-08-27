59f81da82ec32380117ce4377824f3f917161db1
Spaceghost <Spaceghost@users.noreply.github.com>
2026-08-27 13:26:30 -0700

    build: make GMU toolchain aliases relocatable
    (cherry picked from commit fd2cacc4492cd473c95b8c1d2551e9e399193014)

--- FunKey/package/gmu/gmu.mk
+++ FunKey/package/gmu/gmu.mk
@@ -12,14 +12,25 @@ GMU_LICENSE_FILES = LICENSE
 
 GMU_DEPENDENCIES = sdl
 
+define GMU_PREPARE_TOOLCHAIN_ALIASES
+	rm -rf $(@D)/toolchain
+	mkdir -p $(@D)/toolchain/bin
+	for tool in $(HOST_DIR)/bin/arm-linux-*; do \
+		[ -e "$$tool" ] || continue; \
+		name=$${tool##*/arm-linux-}; \
+		ln -s "$$tool" "$(@D)/toolchain/bin/arm-funkey-linux-musleabihf-$$name"; \
+	done
+endef
+
 define GMU_BUILD_CMDS
+	$(GMU_PREPARE_TOOLCHAIN_ALIASES)
 	(cd $(@D); \
 	sed -i -e 's|rm -rf|#rm -rf|g' package; \
 	sed -i -e 's|make -f Makefile.funkey clean|#make -f Makefile.funkey clean|g' package; \
 	chmod +x package; \
-	PATH=/opt/FunKey-sdk/bin:$(PATH) \
-	PKG_CONFIG_SYSROOT_DIR=/opt/FunKey-sdk/arm-funkey-linux-musleabihf/sysroot \
-	PKG_CONFIG_LIBDIR=/opt/FunKey-sdk/arm-funkey-linux-musleabihf/sysroot/usr/lib/pkgconfig:/opt/FunKey-sdk/arm-funkey-linux-musleabihf/sysroot/usr/share/pkgconfig \
+	PATH=$(@D)/toolchain/bin:$(HOST_DIR)/bin:$(PATH) \
+	PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) \
+	PKG_CONFIG_LIBDIR=$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig \
 	./package \
 	)
 endef
